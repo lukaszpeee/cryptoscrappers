dbde3fb30b3921a3c427d2350d35f4272190a81e
lukaszpeee <popielski9666@gmail.com>
2022-08-29 18:39:43 +0200

    Added two new Scrappers. ScrapperTagCrypto and ScrapperTagsCrypto use up and low letters.

---
+++ scrappers/ScrapperProfile.py
@@ -0,0 +1,19 @@
+import snscrape.modules.twitter as sntwitter
+from datetime import date
+from typing import List
+import pandas as pd
+
+
+class ScrapperProfile:
+    def __init__(self, profile: str, since_date: date, until_date: date):
+        self.profile = profile
+        self.since_date = since_date
+        self.until_date = until_date
+        self.tweets_profiles = []
+
+    def start_scrapping(self):
+        query = f"(from:{self.profile}) until:{self.until_date} since:{self.since_date}"
+        for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+            self.tweets_profiles.append([tweet.content])
+        df = pd.DataFrame(self.tweets_profiles, columns=['Tweet'])
+        print(df)
--- scrappers/ScrapperTagCrypto.py
+++ scrappers/ScrapperTagCrypto.py
@@ -1,22 +1,20 @@
-from data.dictionaries import tagscrypto
 from datetime import date
 from typing import List
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 
 
-class ScrapperTagsCrypto:
-    def __init__(self, tagscrypto: List, since_date: date, until_date: date):
-        self.tagscrypto = [x.lower() for x in tagscrypto] + [x.upper() for x in tagscrypto]
+class ScrapperTagCrypto:
+    def __init__(self, tagcrypto: str, since_date: date, until_date: date):
+        self.tagcrypto = [tagcrypto.upper(), tagcrypto.lower()]
         self.since_date = since_date
         self.until_date = until_date
         self.tweets_crypto = []
         
     def start_scrapping(self):
-        for tag in self.tagscrypto:
+        for tag in self.tags:
             query = f"(#${tag}) until:{self.until_date} since:{self.since_date}"
             for tweet in sntwitter.TwitterSearchScraper(query).get_items():
                 self.tweets_crypto.append([tweet.content])
         df = pd.DataFrame(self.tweets_crypto, columns=['Tweet'])
         print(df)
- 
