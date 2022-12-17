bf44d8be2bdd4fd473c38dbb9168500da1450c89
lukaszpeee <popielski9666@gmail.com>
2022-12-17 15:03:15 +0100

    Refactored to shorter version.

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import time
 
-from raports.usecase import main_mergedraport, main_mergedraport_year_before_hossa
-from scrappers.ScrapperProfiles.usecase import main_scrapper_profiles_content_and_author, \
-    main_scrapper_profiles_for_months, main_scrapper_profiles_for_month
+from raports.usecase import main_mergedraport
+
 
 if __name__ == "__main__":
     path = 'C:/Users/rogal/Desktop/Kryptowaluty/raporty_profile/'
     start_time = time.time()
-    # main_scrapper_profiles_for_month(path)
-    # main_scrapper_tags()
-    # main_scrapper_profiles_about()
-    # main_scrapper_profiles_for_month(path)
-    # main_scrapper_profiles_content_and_author(path)
-    main_mergedraport(path)
-    # main_mergedraport_year_before_hossa(path)
     print(f"{(time.time() - start_time)/60} mins")
