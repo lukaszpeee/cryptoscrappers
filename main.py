cb3502b1c83e5b89ec0c28e8c909c41645dd8e60
lukaszpeee <popielski9666@gmail.com>
2023-01-02 21:18:32 +0100

    create_full_content_raport - fixed problem with date.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import time
 
-from raports.usecase import main_mergedraport
+from scrappers.ScrapperProfiles.usecase import main_scrapper_profiles_for_month, \
+    main_scrapper_tweets_date_author_content_for_month
 
 
 if __name__ == "__main__":
     path = 'C:/Users/rogal/Desktop/Kryptowaluty/raporty_profile/'
     start_time = time.time()
+    # main_scrapper_profiles_for_month(path)
+    main_scrapper_tweets_date_author_content_for_month(path)
     print(f"{(time.time() - start_time)/60} mins")
--- raports/FullContentRaport.py
+++ raports/FullContentRaport.py
@@ -11,5 +11,6 @@ class FullContentRaport:
         self.raport_path = raport_path
 
     def create_full_content_raport(self) -> DataFrame:
-        df_content = pd.DataFrame(self.scrapper_data, columns=['Data', 'User', 'Tweet'])
+        df_content = pd.DataFrame(self.scrapper_data, columns=['Date', 'User', 'Tweet'])
+        df_content['Date'] = df_content['Date'].apply(lambda a: pd.to_datetime(a).date())
         df_content.to_excel(self.raport_path)
